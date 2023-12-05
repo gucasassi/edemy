7a0c845eeff652fc212ff3f0faadc6abbaaaa7bb
Guillem Casas <gucasassi@gmail.com>
2023-12-05 22:41:06 +0100

    chore(back): add user model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+// Imports
+import { Schema, model } from "mongoose";
+
+// Create schema
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    picture: {
+      type: String,
+      default: "/avatar.png",
+    },
+    role: {
+      type: [String],
+      default: ["Subscriber"],
+      enum: ["Subscriber", "Instructor", "Admin"],
+    },
+  },
+  { timestamps: true } // Create automatically createdAt / updatedAt
+);
+
+// Export model
+export default model("User", userSchema);
