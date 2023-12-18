0db992877d8ffd0cfbc1cb1bc291977989374615
Guillem Casas <gucasassi@gmail.com>
2023-12-18 16:22:22 +0100

    feat(auth): add register endpoint

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,12 @@
-export const register = (req, res) => {
-  res.json({ message: "register user" });
+// Imports
+import User from "../models/User.js";
+
+// @desc    Register new user
+// @route   POST /api/v1/auth/register
+// @access  Public
+export const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({ name, email, password });
+
+  res.status(200).json({ success: true, data: user });
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,37 +1,52 @@
 // Imports
+import bcrypt from "bcryptjs";
 import { Schema, model } from "mongoose";
 
-// Create schema
-const userSchema = new Schema(
+// Create User schema
+const UserSchema = new Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: true,
+      required: [true, "Name is required"],
     },
     email: {
       type: String,
       trim: true,
       unique: true,
-      required: true,
+      required: [true, "Email is required"],
+      match: [/.+\@.+\..+/, "Please fill a valid email address"], // Validate email format
     },
     password: {
       type: String,
-      trim: true,
-      required: true,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"], // Minimum password length
     },
     picture: {
       type: String,
-      default: "/avatar.png",
+      default: "/avatar.png", // Default avatar
     },
     role: {
-      type: [String],
-      default: ["Subscriber"],
-      enum: ["Subscriber", "Instructor", "Admin"],
+      type: String,
+      default: "Subscriber",
+      enum: {
+        values: ["Subscriber", "Instructor", "Admin"],
+        message: "{VALUE} is not a supported role", // Custom message for invalid role
+      },
     },
   },
-  { timestamps: true } // Create automatically createdAt / updatedAt
+  { timestamps: true } // Automatically create createdAt and updatedAt timestamps
 );
 
-// Export model
-export default model("User", userSchema);
+// Encrypt password using bcrypt before saving
+UserSchema.pre("save", async function (next) {
+  // Only encrypt the password if it has been modified (or is new)
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Export the User model
+export default model("User", UserSchema);
